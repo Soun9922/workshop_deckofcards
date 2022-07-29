b24b11900e52f2f1a09ae470e88a07d0b99dabbb
Soun9922 <sagsounak39@gmail.com>
2022-07-29 11:38:31 +0530

    The code for deck of cards is added

---
+++ src/deckofcards/DeckOfCards.java
@@ -0,0 +1,36 @@
+package deckofcards;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
+                            "Queen", "King", "Ace"};
+
+        int n = suits.length * ranks.length;
+        String[] deck = new String[n];
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                deck[suits.length * i + j] = ranks[i] + " of " + suits[j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n - i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(deck[i]);
+//        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println();
+            System.out.println("**Person " + (i + 1) + "**");
+            for (int j = 0; j < 9; j++) {
+                System.out.println(deck[i + j * 4]);
+            }
+        }
+    }
+}
